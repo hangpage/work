7ff1e3959189fcc4782c99975f8969b1f48c926d
zhuzhihang <zzhihang@hotmail.com>
2020-07-09 10:38:39 +0800

    update

--- src/components/RichInput.js
+++ src/components/RichInput.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import config from "../utils/config";
 
 export default class RichInput extends React.Component {
   constructor(props) {
     super(props);
+    this.reactQuillRef = null;
     this.state = {
       value: '',
       readOnly: this.props.disabled
+    }
+  }
+
+  //这是点击图片图标触发的事件
+  imageHandler() {
+    const input = document.createElement('input')
+    input.setAttribute('type', 'file')
+    input.setAttribute('accept', 'image/*')
+    input.setAttribute('multiple', 'multiple')
+    input.click()
+    const that = this;
+    input.onchange = async () => {
+      Array.from(input.files).forEach(item => {
+        const formData = new FormData();
+        formData.append('file', item);
+        fetch('/upload', {
+          method: 'post',
+          body: formData,
+        }).then(response => response.json())
+          .then((data) => {debugger
+            let quill = that.reactQuillRef.getEditor();//获取到编辑器本身
+            const cursorPosition = quill.getSelection().index;//获取当前光标位置
+            const link = config.UPLOAD_URL + data.data.url;
+            quill.insertEmbed(cursorPosition, "image", link);//插入图片
+            quill.setSelection(cursorPosition + 1);//光标位置加1
+          });
+      })
     }
   }
 
@@ -19,6 +48,10 @@ export default class RichInput extends React.Component {
       ['link', 'image'],
       ['clean']
     ],
+    handlers: {
+      'image': this.imageHandler.bind(this), //点击图片标志会调用的方法
+      'video2': this.showVideoModal.bind(this),
+    },
   };
 
   componentWillReceiveProps(nextProps, nextContext) {
@@ -44,9 +77,11 @@ export default class RichInput extends React.Component {
     }
   };
 
+
+
   render() {
     return (
-      <ReactQuill modules={this.modules} formats={this.formats} value={this.state.value} onChange={this.handleChange}/>
+      <ReactQuill ref={(el) => { this.reactQuillRef = el }} modules={this.modules} formats={this.formats} value={this.state.value} onChange={this.handleChange}/>
     )
   }
 }
